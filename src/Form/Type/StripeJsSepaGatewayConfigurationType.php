aaccdad0b5bf77f97486a5401faf7a353424a0d5
Thomas Billot <thomas.billot@awstudio.fr>
2023-11-10 12:26:11 +0100

    Added gateway configuration type

--- src/Form/Type/StripeJsSepaGatewayConfigurationType.php
+++ src/Form/Type/StripeJsSepaGatewayConfigurationType.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Motherbrain\SyliusPayumStripeSepaPlugin\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\NotBlank;
