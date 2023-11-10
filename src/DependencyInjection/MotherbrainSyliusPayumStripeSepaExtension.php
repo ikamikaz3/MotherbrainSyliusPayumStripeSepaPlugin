070b3cbe9e9227b6fdb9e74c040f5f28e6ca6e75
Thomas Billot <thomas.billot@awstudio.fr>
2023-11-10 14:48:48 +0100

    (fix) Changed file loader

--- src/DependencyInjection/MotherbrainSyliusPayumStripeSepaExtension.php
+++ src/DependencyInjection/MotherbrainSyliusPayumStripeSepaExtension.php
@@ -9,7 +9,7 @@ use Sylius\Bundle\ResourceBundle\DependencyInjection\Extension\AbstractResourceE
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 final class MotherbrainSyliusPayumStripeSepaExtension extends AbstractResourceExtension implements PrependExtensionInterface
 {
@@ -18,7 +18,7 @@ final class MotherbrainSyliusPayumStripeSepaExtension extends AbstractResourceEx
     /** @psalm-suppress UnusedVariable */
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
 
         $loader->load('services.yaml');
     }
